69a39c7bce7796107b59268dea6290b6eb00c7c9
sanjam00 <sanaya.j.james@gmail.com>
2025-06-10 22:25:37 -0500

    Add files via upload

---
+++ rock_paper_scissors.py
@@ -0,0 +1,43 @@
+import random
+
+print("==============================")
+print("     Rock, Paper, Scissors!")
+print("==============================")
+print("")
+
+print("Type the number corresponding to rock, paper, or scissors.")
+print("1) Rock")
+print("2) Paper")
+print("3) Scissors")
+
+player = int(input("Which do you choose? "))
+computer = random.randint(1, 3)
+
+print("")
+print("====================")
+if player == 1 and computer == 3:
+    choices = {1: "rock", 2: "paper", 3: "scissors" }
+    print(f"You chose: {choices[player]}")
+    print(f"CPU chose: {choices[computer]}")
+    print("Player wins!")
+elif player == 2 and computer == 1:
+    choices = {1: "rock", 2: "paper", 3: "scissors" }
+    print(f"You chose: {choices[player]}")
+    print(f"CPU chose: {choices[computer]}")
+    print("Player wins!")
+elif player == 3 and computer == 2:
+    choices = {1: "rock", 2: "paper", 3: "scissors" }
+    print(f"You chose: {choices[player]}")
+    print(f"CPU chose: {choices[computer]}")
+    print("Player wins!")
+elif player == computer:
+    choices = {1: "rock", 2: "paper", 3: "scissors" }
+    print(f"You chose: {choices[player]}")
+    print(f"CPU chose: {choices[computer]}")
+    print("It's a tie!")
+else:
+    choices = {1: "rock", 2: "paper", 3: "scissors" }
+    print(f"You chose: {choices[player]}")
+    print(f"CPU chose: {choices[computer]}")
+    print("CPU wins!")
+print("====================")
