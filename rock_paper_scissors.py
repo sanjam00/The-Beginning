d5bc868439830ddbef2b493ca122ecba4ab957ae
sanjam00 <sanaya.j.james@gmail.com>
2025-06-27 14:17:26 -0500

    rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -13,30 +13,27 @@ print("3) Scissors")
 player = int(input("Which do you choose? "))
 computer = random.randint(1, 3)
 
+choices = {1: "rock", 2: "paper", 3: "scissors" }
+
 print("")
 print("====================")
 if player == 1 and computer == 3:
-    choices = {1: "rock", 2: "paper", 3: "scissors" }
     print(f"You chose: {choices[player]}")
     print(f"CPU chose: {choices[computer]}")
     print("Player wins!")
 elif player == 2 and computer == 1:
-    choices = {1: "rock", 2: "paper", 3: "scissors" }
     print(f"You chose: {choices[player]}")
     print(f"CPU chose: {choices[computer]}")
     print("Player wins!")
 elif player == 3 and computer == 2:
-    choices = {1: "rock", 2: "paper", 3: "scissors" }
     print(f"You chose: {choices[player]}")
     print(f"CPU chose: {choices[computer]}")
     print("Player wins!")
 elif player == computer:
-    choices = {1: "rock", 2: "paper", 3: "scissors" }
     print(f"You chose: {choices[player]}")
     print(f"CPU chose: {choices[computer]}")
     print("It's a tie!")
 else:
-    choices = {1: "rock", 2: "paper", 3: "scissors" }
     print(f"You chose: {choices[player]}")
     print(f"CPU chose: {choices[computer]}")
     print("CPU wins!")
